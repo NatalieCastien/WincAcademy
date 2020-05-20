bc9edab202e4eebd6dd136b00bb0bb537f31a0a7
NatalieCastien <natalie.castien@gmail.com>
2020-05-20 09:46:35 +0200

    Dag3 opdracht 1

---
+++ Week3/Dag3/W3D3-1/index.js
@@ -0,0 +1,9 @@
+// 1
+const sum = (...numbers) => numbers.reduce((prev, cur) => prev + cur);
+console.log(sum(1,2,3,4,5));
+
+
+// 2
+const sum2 = (num1, num2, num3) => num1 +  num2 + num3;
+const numbers = [3,4,5];
+console.log(sum2(...numbers));
