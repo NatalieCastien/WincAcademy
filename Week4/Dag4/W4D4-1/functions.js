bdc18c43f4b0fb1df1ca5fbdc54045723b139f99
NatalieCastien <natalie.castien@gmail.com>
2020-05-29 09:53:40 +0200

    update pw verifier - removes comments

--- Week4/Dag4/W4D4-1/functions.js
+++ Week4/Dag4/W4D4-1/functions.js
@@ -39,44 +39,7 @@ const functions = {
   },
   isValid: (password) => {
     let score = 0;
-    // const isNotBlank = (pw) => {
-    //   if (pw.length > 0) {
-    //     return true;
-    //   } else {
-    //     return false;
-    //   }
-    // };
-    // const isLessThan8Chars = (pw) => {
-    //   if (pw.length <= 8) {
-    //     return true;
-    //   } else {
-    //     return false;
-    //   }
-    // },
-    // const hasUppercase = (pw) => {
-    //     const regexUppercase = /[A-Z]+/;
-    //     if (pw.match(regexUppercase)) {
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    // };
-    // const hasLowercase = (pw) => {
-    //     const regexLowercase = /[a-z]+/;
-    //     if (pw.match(regexLowercase)) {
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    // };
-    // const hasDigit = (pw) => {
-    //     const regexDigit = /[0-9]+/;
-    //     if (pw.match(regexDigit)) {
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    // };
+
     if (functions.isNotBlank(password)) score++;
     if (functions.isLessThan8Chars(password)) score++;
     if (functions.hasUppercase(password)) score++;
@@ -94,10 +57,5 @@ const functions = {
   },
 };
 
-// console.log(functions.isLessThan8Chars(""));
-// console.log(functions.isNotBlank(""));
-// console.log(functions.hasUppercase(""));
-// console.log(functions.hasLowercase(""));
-// console.log(functions.hasDigit(""));
 console.log(functions.isValid("henkie1"));
 module.exports = functions;
