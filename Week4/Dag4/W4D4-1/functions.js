204b99a7cff4f8ddb5327820754421ba75ac2fc2
NatalieCastien <natalie.castien@gmail.com>
2020-05-29 09:49:35 +0200

    password verifier 1st commit

--- Week4/Dag4/W4D4-1/functions.js
+++ Week4/Dag4/W4D4-1/functions.js
@@ -37,11 +37,67 @@ const functions = {
       return false;
     }
   },
+  isValid: (password) => {
+    let score = 0;
+    // const isNotBlank = (pw) => {
+    //   if (pw.length > 0) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // };
+    // const isLessThan8Chars = (pw) => {
+    //   if (pw.length <= 8) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // },
+    // const hasUppercase = (pw) => {
+    //     const regexUppercase = /[A-Z]+/;
+    //     if (pw.match(regexUppercase)) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    // };
+    // const hasLowercase = (pw) => {
+    //     const regexLowercase = /[a-z]+/;
+    //     if (pw.match(regexLowercase)) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    // };
+    // const hasDigit = (pw) => {
+    //     const regexDigit = /[0-9]+/;
+    //     if (pw.match(regexDigit)) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    // };
+    if (functions.isNotBlank(password)) score++;
+    if (functions.isLessThan8Chars(password)) score++;
+    if (functions.hasUppercase(password)) score++;
+    if (functions.hasDigit(password)) score++;
+    if (functions.hasLowercase(password)) {
+      score++;
+    } else {
+      score = 0;
+    }
+
+    return {
+      score: score,
+      valid: score >= 3,
+    };
+  },
 };
 
-console.log(functions.isLessThan8Chars(""));
-console.log(functions.isNotBlank(""));
-console.log(functions.hasUppercase(""));
-console.log(functions.hasLowercase(""));
-console.log(functions.hasDigit(""));
+// console.log(functions.isLessThan8Chars(""));
+// console.log(functions.isNotBlank(""));
+// console.log(functions.hasUppercase(""));
+// console.log(functions.hasLowercase(""));
+// console.log(functions.hasDigit(""));
+console.log(functions.isValid("henkie1"));
 module.exports = functions;
--- Week4/Dag4/W4D4-1/functions.test.js
+++ Week4/Dag4/W4D4-1/functions.test.js
@@ -2,61 +2,89 @@ const functions = require("./functions");
 
 test("Should not be blank", () => {
   const pw = "b";
-  expect(functions.isNotBlank(pw)).toBe(true);
+  const expected = true;
+  expect(functions.isNotBlank(pw)).toBe(expected);
 });
 test("Should be blank", () => {
   const pw = "";
-  expect(functions.isNotBlank(pw)).toBe(false);
+  const expected = false;
+  expect(functions.isNotBlank(pw)).toBe(expected);
 });
 
 test("Should not be more than 8 chars", () => {
   const pw = "a1b2c3d4";
-  expect(functions.isLessThan8Chars(pw)).toBe(true);
+  const expected = true;
+  expect(functions.isLessThan8Chars(pw)).toBe(expected);
 });
 test("Should be more than 8 chars", () => {
   const pw = "a1b2c3d4e";
-  expect(functions.isLessThan8Chars(pw)).toBe(false);
+  const expected = false;
+  expect(functions.isLessThan8Chars(pw)).toBe(expected);
 });
 
 test("Has uppercase letters", () => {
   const pw = "Abcd1";
   const regex = /[A-Z]+/;
   expect(pw).toMatch(regex);
-  expect(functions.hasUppercase(pw)).toBe(true);
+  const expected = true;
+  expect(functions.hasUppercase(pw)).toBe(expected);
 });
 test("Has no uppercase letters", () => {
   const pw = "abcd1";
   const regex = /[A-Z]+/;
+  const expected = false;
   expect(pw).not.toMatch(regex);
-  expect(functions.hasUppercase(pw)).toBe(false);
+  expect(functions.hasUppercase(pw)).toBe(expected);
 });
 
 test("Has lowercase letters", () => {
   const pw = "ABCd1";
   const regex = /[a-z]+/;
+  const expected = true;
   expect(pw).toMatch(regex);
-  expect(functions.hasLowercase(pw)).toBe(true);
+  expect(functions.hasLowercase(pw)).toBe(expected);
 });
 test("Has no lowercase letters", () => {
   const pw = "AB3E";
   const regex = /[a-z]+/;
+  const expected = false;
   expect(pw).not.toMatch(regex);
-  expect(functions.hasLowercase(pw)).toBe(false);
+  expect(functions.hasLowercase(pw)).toBe(expected);
 });
 
 test("Has digit", () => {
   const pw = "Ab1cd";
   const regex = /[0-9]+/;
+  const expected = true;
   expect(pw).toMatch(regex);
-  expect(functions.hasDigit(pw)).toBe(true);
+  expect(functions.hasDigit(pw)).toBe(expected);
 });
 test("Has no digits", () => {
   const pw = "AbcdE";
   const regex = /[0-9]+/;
+  const expected = false;
   expect(pw).not.toMatch(regex);
-  expect(functions.hasDigit(pw)).toBe(false);
+  expect(functions.hasDigit(pw)).toBe(expected);
 });
 
+// To test a bunch of passwords
+const pwArray = [
+  { pw: "henkie1", ex: { score: 4, valid: true } },
+  { pw: "1234a", ex: { score: 4, valid: true } },
+  { pw: "z", ex: { score: 3, valid: true } },
+  { pw: "henkie1234", ex: { score: 3, valid: true } },
+  { pw: "HENKhenk", ex: { score: 4, valid: true } },
+  { pw: "HENK33$", ex: { score: 0, valid: false } },
+  { pw: "1234", ex: { score: 0, valid: false } },
+  { pw: "", ex: { score: 0, valid: false } },
+];
+
 test("Is password verified", () => {
-  const pw = "";
+  pwArray.forEach((pw) => {
+    let password = pw.pw;
+    let expected = pw.ex;
+    console.log(password);
+    console.log(expected);
+    expect(functions.isValid(password)).toEqual(expected);
+  });
 });
